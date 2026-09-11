b464304d1f2ca4eca3d95a71b9c5d9ac01018eee
bbogdan59 <129838289+bbogdan59@users.noreply.github.com>
2026-09-11 07:52:08 +0200

    Merge enrollment and telemetry Alembic heads

---
+++ alembic/versions/c91a17e4b208_merge_enrollment_telemetry.py
@@ -0,0 +1,14 @@
+"""Merge independently developed enrollment and telemetry migrations."""
+
+revision = "c91a17e4b208"
+down_revision = ("a3f7c9d1e6b2", "b8e2f4a1c9d7")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
